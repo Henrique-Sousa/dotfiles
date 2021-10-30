7468706bc7ead3ebd55792b2cf3cce383d4c4b33
Henrique-Sousa <29417378+Henrique-Sousa@users.noreply.github.com>
2021-10-30 07:21:05 -0300

    make ppm install handle multiple or zero arguments

--- .scripts/ppm.sh
+++ .scripts/ppm.sh
@@ -1,10 +1,14 @@
 ppm() {
   if [ $1 = install ]; then
-    pip3 install $2 &&
+    if [ $# -gt 1 ]; then
+      pip3 install ${@:2};
+    else
+      pip3 install -r requirements.txt;
+    fi 
     pip3 freeze > $(pwd)/requirements.txt &&
     python3 -m pipdeptree > deptree.txt;
-  elif [ $1 = uninstall ]; then
-    pip3 uninstall $2 &&
+  elif [ $1 = uninstall -a $# -gt 1 ]; then
+    pip3 uninstall ${@:2} &&
     pip3 freeze > $(pwd)/requirements.txt &&
     python3 -m pipdeptree > deptree.txt;
   fi
