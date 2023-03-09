8786b08057c01ca25706a5600313fd5b288c39ba
Henrique-Sousa <29417378+Henrique-Sousa@users.noreply.github.com>
2023-03-09 14:27:03 -0300

    add russian and greek

--- .scripts/toggle_keyboard_layout.sh
+++ .scripts/toggle_keyboard_layout.sh
@@ -2,6 +2,10 @@
 
 if [ $(setxkbmap -query | grep layout | awk -F':     ' '{print $2}') = us ]; then
     setxkbmap -layout br
+elif [ $(setxkbmap -query | grep layout | awk -F':     ' '{print $2}') = br ]; then
+    setxkbmap -layout gr 
+elif [ $(setxkbmap -query | grep layout | awk -F':     ' '{print $2}') = gr ]; then
+    setxkbmap -layout ru
 else
-    setxkbmap -layout us 
+    setxkbmap -layout us
 fi
