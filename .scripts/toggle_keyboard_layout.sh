44f1a75c6841f157f07490a82cc2ff70e949ee9d
Henrique-Sousa <29417378+Henrique-Sousa@users.noreply.github.com>
2023-03-12 15:14:13 -0300

    remove gr and ru from keyboard layouts

--- .scripts/toggle_keyboard_layout.sh
+++ .scripts/toggle_keyboard_layout.sh
@@ -3,9 +3,7 @@
 if [ $(setxkbmap -query | grep layout | awk -F':     ' '{print $2}') = us ]; then
     setxkbmap -layout br
 elif [ $(setxkbmap -query | grep layout | awk -F':     ' '{print $2}') = br ]; then
-    setxkbmap -layout gr 
-elif [ $(setxkbmap -query | grep layout | awk -F':     ' '{print $2}') = gr ]; then
-    setxkbmap -layout ru
+    setxkbmap -layout us
 else
     setxkbmap -layout us
 fi
