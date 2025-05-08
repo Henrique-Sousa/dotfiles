26673d1589d60a808e5569c13139b5cab50889fc
Henrique-Sousa <29417378+Henrique-Sousa@users.noreply.github.com>
2025-05-08 17:31:53 -0300

    lock the screen before suspending

--- .scripts/suspend.sh
+++ .scripts/suspend.sh
@@ -1,3 +1,14 @@
 #!/bin/sh
 
-systemctl suspend; slock
+# Lock screen first
+slock &
+
+# Give slock time to grab the X server
+sleep 0.5
+
+# Flush any keyboard events that might wake it instantly
+xset dpms force off
+sleep 0.5
+
+# Then suspend
+systemctl suspend
