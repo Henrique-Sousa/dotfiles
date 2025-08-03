28ee88b29e9f44c90c0ff12367aada96db13b7eb
Henrique-Sousa <29417378+Henrique-Sousa@users.noreply.github.com>
2025-08-03 20:42:13 -0300

    set alacritty

--- .local/src/dwm/config.h
+++ .local/src/dwm/config.h
@@ -56,7 +56,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "alacritty", NULL };
 static const char *screenshot[]  = { "scrot", "dwnlds/screenshots/%Y-%m-%d-%H%M%S.png", NULL };
 static const char *screenshots_region[]  = { "scrot", "-s", "dwnlds/screenshots/%Y-%m-%d-%H%M%S.png", NULL };
 static const char *suspendcmd[]  = {"/home/user1/.local/bin/suspend.sh", NULL };
