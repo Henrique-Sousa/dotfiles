99f5f8774de32ff19350b532099781a37e602c3b
Henrique-Sousa <29417378+Henrique-Sousa@users.noreply.github.com>
2025-08-10 16:48:08 -0300

    fix bug: rus -> br now is rus -> us; refactor

--- .local/bin/toggle_keyboard_layout.sh
+++ .local/bin/toggle_keyboard_layout.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/sh
 
-if [ $(setxkbmap -query | grep layout | awk -F':     ' '{print $2}') = us ]; then
+layout=$(setxkbmap -query | awk '/layout/ {print $2}')
+variant=$(setxkbmap -query | awk '/variant/ {print $2}')
+
+if [ "$layout" == us -a "$variant" != rus ]; then
     setxkbmap -layout br
-elif [ $(setxkbmap -query | grep layout | awk -F':     ' '{print $2}') = br ]; then
-    setxkbmap -layout us
 else
     setxkbmap -layout us
 fi
