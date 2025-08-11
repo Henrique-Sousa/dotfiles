705b7cac253e3b2807403f36fae9fdfd759d00f9
Henrique-Sousa <29417378+Henrique-Sousa@users.noreply.github.com>
2025-08-11 13:35:06 -0300

    go back to simpler script

--- .local/bin/suspend.sh
+++ .local/bin/suspend.sh
@@ -1,14 +1,4 @@
 #!/bin/sh
 
-# Lock screen first
-slock &
-
-# Give slock time to grab the X server
-sleep 0.5
-
-# Flush any keyboard events that might wake it instantly
-xset dpms force off
-sleep 0.5
-
-# Then suspend
-systemctl suspend
+#sudo systemctl suspend && slock
+systemctl suspend && slock
