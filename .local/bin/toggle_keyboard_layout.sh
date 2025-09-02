81e4408fa2e059dda62b5da6a5c6d21c81d160af
Henrique-Sousa <29417378+Henrique-Sousa@users.noreply.github.com>
2025-09-02 20:29:47 -0300

    make script posix compliant to run faster with dash

--- .local/bin/toggle_keyboard_layout.sh
+++ .local/bin/toggle_keyboard_layout.sh
@@ -3,7 +3,7 @@
 layout=$(setxkbmap -query | awk '/layout/ {print $2}')
 variant=$(setxkbmap -query | awk '/variant/ {print $2}')
 
-if [ "$layout" == us -a "$variant" != rus ]; then
+if [ "$layout" = us -a "$variant" != rus ]; then
     setxkbmap -layout br
 else
     setxkbmap -layout us
