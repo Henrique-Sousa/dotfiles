37e3d3a66f474282502993096b2afa05a43ef427
Henrique-Sousa <29417378+Henrique-Sousa@users.noreply.github.com>
2025-09-29 18:42:19 -0300

    nerdtree config file from vim to lua

---
+++ .config/nvim/after/plugin/nerdtree.lua
@@ -0,0 +1,4 @@
+vim.keymap.set('n', '<leader>nf', vim.cmd.NERDTreeFocus, { noremap = true })
+vim.keymap.set('n', '<leader>n', vim.cmd.NERDTree, { noremap = true })
+vim.keymap.set('n', '<leader>nt', vim.cmd.NERDTreeToggle, { noremap = true })
+vim.keymap.set('n', '<leader>nF', vim.cmd.NERDTreeFind , { noremap = true })
