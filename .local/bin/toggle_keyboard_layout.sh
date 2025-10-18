ea21befd58dde57ee42f23b4be36172aa4a99ff7
Henrique-Sousa <29417378+Henrique-Sousa@users.noreply.github.com>
2025-10-18 18:16:48 -0300

    all keyboard layouts now it super + space (cycling)

--- .local/bin/toggle_keyboard_layout.sh
+++ .local/bin/toggle_keyboard_layout.sh
@@ -3,8 +3,16 @@
 layout=$(setxkbmap -query | awk '/layout/ {print $2}')
 variant=$(setxkbmap -query | awk '/variant/ {print $2}')
 
-if [ "$layout" = us -a "$variant" != rus ]; then
+if [ "$layout" = us ] && [ "$variant" != rus ] && [ "$variant" != colemak ] && [ "$variant" != dvp ]; then
     setxkbmap -layout br
-else
+elif [ "$layout" = br ]; then
+    setxkbmap -layout gr -variant polytonic
+elif [ "$layout" = gr ]; then
+    setxkbmap -layout us -variant rus
+elif [ "$layout" = us ] && [ "$variant" = rus ]; then
+    setxkbmap -layout us -variant colemak
+elif [ "$layout" = us ] && [ "$variant" = colemak ]; then
+    setxkbmap -layout us -variant dvp
+elif [ "$layout" = us ] && [ "$variant" = dvp ]; then   # programmer dvorak
     setxkbmap -layout us
 fi
