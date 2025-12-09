0e2fe58c8ae43b14deed4aceeb159e6351a932f7
Henrique-Sousa <29417378+Henrique-Sousa@users.noreply.github.com>
2025-12-09 16:05:57 -0300

    keymapping go to open quickfix file without leaving the window

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,2 +1,10 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>x", vim.cmd.Ex)
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "qf",
+  callback = function()
+    -- Map go to open the file and immediately return to the quickfix window
+    vim.api.nvim_buf_set_keymap(0, "n", "go", "<CR><c-w>p", { noremap = true, silent = true })
+  end,
+})
