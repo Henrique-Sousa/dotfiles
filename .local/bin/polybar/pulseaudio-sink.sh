d5e90a73a7e1062ee3ad2b08de49af82d64fce47
Henrique-Sousa <29417378+Henrique-Sousa@users.noreply.github.com>
2026-02-10 19:42:13 -0300

    laptop [audio] -> speakers

--- .local/bin/polybar/pulseaudio-sink.sh
+++ .local/bin/polybar/pulseaudio-sink.sh
@@ -3,7 +3,7 @@
 sink=$(pactl get-default-sink)
 
 case "$sink" in
-    "alsa_output.pci-0000_00_1f.3.analog-stereo")   echo "laptop" ;;
+    "alsa_output.pci-0000_00_1f.3.analog-stereo")   echo "speakers" ;;
     "alsa_output.pci-0000_00_1f.3.hdmi-stereo")     echo "hdmi" ;;
     *)                                              echo "$sink" ;;
 esac
