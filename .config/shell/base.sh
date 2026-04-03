772befede597831784de01b7d296fa8771c3d85d
Henrique-Sousa <29417378+Henrique-Sousa@users.noreply.github.com>
2026-04-03 10:49:03 -0300

    style: separate base directories from aplications directories

--- .config/shell/base.sh
+++ .config/shell/base.sh
@@ -6,6 +6,7 @@ export XDG_CONFIG_HOME="$HOME/.config"
 export XDG_CACHE_HOME="$HOME/.cache"
 export XDG_DATA_HOME="$HOME/.local/share"
 export XDG_STATE_HOME="$HOME/.local/state"
+
 export INPUTRC="$XDG_CONFIG_HOME/readline/inputrc"
 export XINITRC="$XDG_CONFIG_HOME/x11/xinitrc"
 export XSERVERRC="$XDG_CONFIG_HOME/x11/xserverrc"
