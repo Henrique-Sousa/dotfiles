7a3a23507b86c651436e772e855b11810ee29027
Henrique-Sousa <29417378+Henrique-Sousa@users.noreply.github.com>
2026-04-07 16:55:51 -0300

    set react and json to 2 spaces indentation

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -17,7 +17,7 @@ vim.opt.expandtab = true
 vim.opt.smartindent = true
 
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = { "cpp", "fortran" },
+  pattern = { "cpp", "fortran", "javascriptreact", "json" },
   callback = function()
     vim.opt_local.tabstop = 2
     vim.opt_local.shiftwidth = 2
