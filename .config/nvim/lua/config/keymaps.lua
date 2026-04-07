cc5994bc16020b26f1f17ed0204c55750d451c1b
Henrique-Sousa <29417378+Henrique-Sousa@users.noreply.github.com>
2026-04-07 16:55:23 -0300

    add some keybindings

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,5 +1,12 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>x", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>q", ':q<CR>')
+vim.keymap.set("n", "<leader>w", ':w<CR>')
+vim.keymap.set("n", "<leader>n", ':bn<CR>')
+vim.keymap.set("n", "<leader>p", ':bp<CR>')
+vim.keymap.set("n", "<leader>h", ':noh<CR>')
+-- vim.keymap.set("n", "<leader>cn", ':cn<CR>')
+-- vim.keymap.set("n", "<leader>cp", ':cp<CR>')
 
 vim.api.nvim_create_autocmd("FileType", {
   pattern = "qf",
