9d68368b3d551cd55059a0a7704b1dbee50eb84e
Henrique-Sousa <29417378+Henrique-Sousa@users.noreply.github.com>
2026-04-09 10:41:30 -0300

    nerdtree: ignore node_modules

--- .config/nvim/after/plugin/nerdtree.lua
+++ .config/nvim/after/plugin/nerdtree.lua
@@ -2,3 +2,5 @@ vim.keymap.set('n', '<leader>nf', vim.cmd.NERDTreeFocus, { noremap = true })
 vim.keymap.set('n', '<leader>l', vim.cmd.NERDTree, { noremap = true })
 vim.keymap.set('n', '<leader>nt', vim.cmd.NERDTreeToggle, { noremap = true })
 vim.keymap.set('n', '<leader>nF', vim.cmd.NERDTreeFind , { noremap = true })
+
+vim.g.NERDTreeIgnore = { '^node_modules$' }
