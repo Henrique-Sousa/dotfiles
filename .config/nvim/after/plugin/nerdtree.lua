6641c34d993c1f6c023daf508d786e95dcc7302f
Henrique-Sousa <29417378+Henrique-Sousa@users.noreply.github.com>
2026-05-10 11:20:51 -0300

    change keybinding for nerd tree

--- .config/nvim/after/plugin/nerdtree.lua
+++ .config/nvim/after/plugin/nerdtree.lua
@@ -1,5 +1,5 @@
 vim.keymap.set('n', '<leader>nf', vim.cmd.NERDTreeFocus, { noremap = true })
-vim.keymap.set('n', '<leader>l', vim.cmd.NERDTree, { noremap = true })
+vim.keymap.set('n', '<leader>nl', vim.cmd.NERDTree, { noremap = true })
 vim.keymap.set('n', '<leader>nt', vim.cmd.NERDTreeToggle, { noremap = true })
 vim.keymap.set('n', '<leader>nF', vim.cmd.NERDTreeFind , { noremap = true })
 
