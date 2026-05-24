877dc45a4b01eedea634c66d0b177af383888cdb
Henrique-Sousa <29417378+Henrique-Sousa@users.noreply.github.com>
2026-05-24 18:33:20 -0300

    ga to git add

--- .config/nvim-lsp/lua/plugins/fugitive.lua
+++ .config/nvim-lsp/lua/plugins/fugitive.lua
@@ -5,7 +5,7 @@ return {
     -- { "<leader>gs", "<cmd>Git status<cr>", desc = "Git status" },
     { "<leader>gd", "<cmd>Gvdiffsplit<cr>", desc = "Git diff split (vertical)" },
     { "<leader>gh", "<cmd>Ghdiffsplit<cr>", desc = "Git diff split (horizontal)" },
-    { "<leader>gw", "<cmd>Git add %<cr>", desc = "Git add current file" },
+    { "<leader>ga", "<cmd>Git add %<cr>", desc = "Git add current file" },
     { "<leader>gc", "<cmd>Git commit<cr>", desc = "Git commit" },
   },
 }
