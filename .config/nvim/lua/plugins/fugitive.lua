f1a0efa6efb05a25e3afd838e4085686fc504b38
Henrique-Sousa <29417378+Henrique-Sousa@users.noreply.github.com>
2026-05-25 00:19:20 -0300

    keybindings for fugitive

--- .config/nvim/lua/plugins/fugitive.lua
+++ .config/nvim/lua/plugins/fugitive.lua
@@ -1,8 +1,8 @@
 return {
   "tpope/vim-fugitive",
   keys = {
-    { "<leader>gs", vim.cmd.Git, desc = "Git" },
-    -- { "<leader>gs", "<cmd>Git status<cr>", desc = "Git status" },
+    { "<leader>gi", vim.cmd.Git, desc = "Git" },
+    { "<leader>gs", "<cmd>Git status<cr>", desc = "Git status" },
     { "<leader>gd", "<cmd>Gvdiffsplit<cr>", desc = "Git diff split (vertical)" },
     { "<leader>gh", "<cmd>Ghdiffsplit<cr>", desc = "Git diff split (horizontal)" },
     { "<leader>ga", "<cmd>Git add %<cr>", desc = "Git add current file" },
