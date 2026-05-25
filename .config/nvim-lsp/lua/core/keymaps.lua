a33edf3dd7e1319e4dfe75a18a9fba9cc7eb1c2c
Henrique-Sousa <29417378+Henrique-Sousa@users.noreply.github.com>
2026-05-24 23:45:38 -0300

    comment

--- .config/nvim-lsp/lua/core/keymaps.lua
+++ .config/nvim-lsp/lua/core/keymaps.lua
@@ -12,6 +12,7 @@ vim.api.nvim_create_autocmd("FileType", {
   pattern = "qf",
   callback = function()
     -- Map go to open the file and immediately return to the quickfix window
+    -- so that I can just preview the errors in each file without leaving quickfix list
     vim.api.nvim_buf_set_keymap(0, "n", "go", "<CR><c-w>p", { noremap = true, silent = true })
   end,
 })
