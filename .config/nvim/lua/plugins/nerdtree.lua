1342fcf93fffc06de65e3eed35d4d3ff1c0d807a
Henrique-Sousa <29417378+Henrique-Sousa@users.noreply.github.com>
2026-05-26 14:22:05 -0300

    comment out some keybindings

--- .config/nvim/lua/plugins/nerdtree.lua
+++ .config/nvim/lua/plugins/nerdtree.lua
@@ -1,10 +1,10 @@
 return {
   "preservim/nerdtree",
   keys = {
-    { "<leader>lf", "<cmd>NERDTreeFocus<cr>", desc = "NERDTree Focus" },
-    { "<leader>ll", "<cmd>NERDTree<cr>", desc = "NERDTree Open" },
+    -- { "<leader>lf", "<cmd>NERDTreeFocus<cr>", desc = "NERDTree Focus" },
+    -- { "<leader>ll", "<cmd>NERDTree<cr>", desc = "NERDTree Open" },
     { "<leader>lt", "<cmd>NERDTreeToggle<cr>", desc = "NERDTree Toggle" },
-    { "<leader>lF", "<cmd>NERDTreeFind<cr>", desc = "NERDTree Find" },
+    { "<leader>lf", "<cmd>NERDTreeFind<cr>", desc = "NERDTree Find" },
   },
   init = function()
     -- The `init` function runs BEFORE the plugin is loaded.
