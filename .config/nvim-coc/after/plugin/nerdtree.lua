38905e1a4229882f590a6b914860428a7fe4b7cf
Henrique-Sousa <29417378+Henrique-Sousa@users.noreply.github.com>
2026-06-01 18:10:10 -0300

    remove nvim-coc

--- .config/nvim-coc/after/plugin/nerdtree.lua
+++
@@ -1,6 +0,0 @@
-vim.keymap.set('n', '<leader>lf', vim.cmd.NERDTreeFocus, { noremap = true })
-vim.keymap.set('n', '<leader>ll', vim.cmd.NERDTree, { noremap = true })
-vim.keymap.set('n', '<leader>lt', vim.cmd.NERDTreeToggle, { noremap = true })
-vim.keymap.set('n', '<leader>lF', vim.cmd.NERDTreeFind , { noremap = true })
-
-vim.g.NERDTreeIgnore = { '^node_modules$' }
