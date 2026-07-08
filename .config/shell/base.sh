4c44563a22b9a0327c493336af1e2a236502f01e
Henrique-Sousa <29417378+Henrique-Sousa@users.noreply.github.com>
2026-07-08 11:29:06 -0300

    config for sdkman

--- .config/shell/base.sh
+++ .config/shell/base.sh
@@ -78,3 +78,7 @@ alias doom="doom emacs -nw"
 stty -ixon
 
 eval $(keychain --eval --quiet --absolute --dir "$XDG_RUNTIME_DIR"/keychain)
+
+#THIS MUST BE AT THE END OF THE FILE FOR SDKMAN TO WORK!!!
+export SDKMAN_DIR="$HOME/.sdkman"
+[[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
